4270e203e87b7fa955b00c26fcd0fbfddb7a906c
0xVe <0xve@protonmail.com>
2022-12-26 22:01:35 +0100

    refactor: 🔥 remove docs

--- docs/components/Container/index.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react'
-import styles from './Container.module.css'
-
-const Container: React.FC<PropsWithChildren> = ({ children }) => {
-  return <div className={styles.container}>{children}</div>
-}
-
-export default Container
