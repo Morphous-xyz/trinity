f7265025dbd3d7edee692e0343a95a7e053fd162
1e18-eth <quentin.jeannot@mutative.co>
2023-05-25 23:11:26 +0200

    removing useless .env

--- tests/utils/constants.ts
+++ tests/utils/constants.ts
@@ -1,20 +1,9 @@
-import * as dotenv from "dotenv";
-dotenv.config();
 
-if (process.env.VITE_ANVIL_FORK_URL === undefined) {
-    throw new Error('Missing environment variable "VITE_ANVIL_FORK_URL"');
-  }
-  
-  export const FORK_URL = process.env.VITE_ANVIL_FORK_URL;
-  
-  if (process.env.VITE_ANVIL_FORK_BLOCK_NUMBER === undefined) {
-    throw new Error('Missing environment variable "VITE_ANVIL_FORK_BLOCK_NUMBER"');
-  }
-  
-  export const FORK_BLOCK_NUMBER = BigInt(Number(process.env.VITE_ANVIL_FORK_BLOCK_NUMBER));
+export const FORK_URL = "https://rpc.tenderly.co/fork/5bdacfe0-ade9-4010-86fa-af654001ff3e"
+export const FORK_BLOCK_NUMBER = BigInt(Number(17327908));
 
 
-  export const PROXY_TEST_ADDRESS = "0x39a4171253F21c211Df595158a320B6E509A6984";
+export const PROXY_TEST_ADDRESS = "0x39a4171253F21c211Df595158a320B6E509A6984";
 
 export const WETH = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
 export const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
